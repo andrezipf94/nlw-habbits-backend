b7a252f22bc4e831192e53093042df98371a472d
Andre Luis Zipf <andrezipf94@gmail.com>
2023-01-29 23:20:50 -0300

    updated host and added a default empty array to completed habits

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,6 @@ app.register(cors);
 app.register(appRoutes);
 
 app.listen({
-    port: 3333
+    port: 3333,
+    host: '0.0.0.0'
 }).then(() => console.log('App running at http://localhost:3333'));
